ae16397b12c102f6f698849b7d72cb08ee64c787
lizwachira <lizwanguwachira@gmail.com>
2024-04-09 05:44:02 +0300

    arrow function

---
+++ index.js
@@ -0,0 +1,9 @@
+// arrow function => should automatically return a value
+let divide = (a, b) => a / b;
+let square = x => x * x;
+let add = (a, b) => a + b;
+
+//output return value
+console.log(divide(10, 2)); //5
+console.log(square(5)); //25
+console.log(add(3, 7)); //10
